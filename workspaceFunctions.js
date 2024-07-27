7a299f0ee5f3e47e65661204ccb4b26c08b70c47
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2024-07-27 15:06:25 +0600

    stable

--- workspaceFunctions.js
+++ workspaceFunctions.js
@@ -10,6 +10,13 @@ var MR_DBUS_IFACE = `
       <method name="GetCurrentWorkspace">
          <arg type="s" direction="out" name="workspaces" />
       </method>
+      <method name="GetWorkspaceIndexByName">
+         <arg type="s" direction="in" name="workspace_name" />
+         <arg type="s" direction="out" name="workspace_num" />
+      </method>
+      <method name="GetWorkspaces">
+         <arg type="s" direction="out" name="workspaces" />
+      </method>
       <method name="GoToGivenWorkspace">
          <arg type="i" direction="in" name="workspace_num" />
       </method>
@@ -24,13 +31,6 @@ var MR_DBUS_IFACE = `
          <arg type="u" direction="in" name="winid" />
          <arg type="i" direction="in" name="workspace_num" />
       </method>
-      <method name="GetWorkspaceIndexByName">
-         <arg type="s" direction="in" name="workspace_name" />
-         <arg type="s" direction="out" name="workspace_num" />
-      </method>
-      <method name="GetWorkspaces">
-         <arg type="s" direction="out" name="workspaces" />
-      </method>
    </interface>
 </node>`;
 
@@ -41,6 +41,83 @@ var WorkspaceFunctions = class WorkspaceFunctions {
     GetCurrentWorkspace() {
         return JSON.stringify(WorkspaceManager.get_active_workspace().index());
 
+    }
+
+    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsWorkspaces org.gnome.Shell.Extensions.GnomeUtilsWorkspaces.GetWorkspaceIndexByName string:"Codium"
+
+    GetWorkspaceIndexByName(workspaceName) {
+
+        // Get the total number of workspaces
+        let number_of_workspaces = global.workspace_manager.n_workspaces;
+
+        // Iterate through each workspace
+        for (let i = 0; i < number_of_workspaces; i++) {
+            // Get the workspace
+            const workspace = WorkspaceManager.get_workspace_by_index(i);
+
+            // Check if the workspace name matches
+            if (Meta.prefs_get_workspace_name(i) == workspaceName) {
+                // Return the index of the workspace
+                return JSON.stringify(i);
+            }
+        }
+    }
+
+    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsWorkspaces org.gnome.Shell.Extensions.GnomeUtilsWorkspaces.GetWorkspaces | jq .
+
+    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsWorkspaces org.gnome.Shell.Extensions.GnomeUtilsWorkspaces.GetWorkspaces | jq -r '.workspace_names[].name'
+
+    GetWorkspaces() {
+        // let w = global.workspace_manager;
+        // return JSON.stringify({
+        //     workspace_number: w.get_n_workspaces(),
+        //     workspace_index: w.get_active_workspace_index(),
+        //     windows: global.screen.get_active_workspace().list_windows(),
+        // });
+
+        let workspaces = []
+        // let current_workspace = Meta.prefs_get_workspace_name(WorkspaceManager.get_active_workspace().index());
+        let number_of_workspaces = global.workspace_manager.n_workspaces;
+        let all_windows_of_workspaces = {};
+        let all_normal_windows_of_workspaces = {};
+        let sticky_windows = [];
+
+        for (let wks = 0; wks < number_of_workspaces; ++wks) {
+
+            // let temp = ;
+
+            workspaces.push({ index: wks, name: Meta.prefs_get_workspace_name(wks) });
+
+
+
+            // let workspace_name = wks+'_'+Meta.prefs_get_workspace_name(wks);
+            let workspace_name = Meta.prefs_get_workspace_name(wks);
+
+            let metaWorkspace = global.workspace_manager.get_workspace_by_index(wks);
+            let all_windows = [];
+
+            metaWorkspace.list_windows().map(w => all_windows.push(w.get_id()));
+
+            // all_windows_of_workspaces.push({ [wks]: all_windows });
+            all_windows_of_workspaces[workspace_name] = all_windows;
+
+            let all_normal_windows = [];
+
+            metaWorkspace.list_windows().filter(w => w.get_window_type() == 0).map(w => all_normal_windows.push(w.get_id()));
+
+            // all_normal_windows_of_workspaces.push({ [wks]: all_normal_windows });
+            all_normal_windows_of_workspaces[workspace_name] = all_normal_windows;
+
+            metaWorkspace.list_windows().filter(w => w.get_window_type() == 0 && !w.is_skip_taskbar() && w.is_on_all_workspaces()).map(w => sticky_windows.push(w.get_id()));
+        }
+
+        return JSON.stringify({
+            workspaces: workspaces,
+            // number_of_workspaces: number_of_workspaces,
+            // current_workspace: current_workspace,
+            // all_windows_of_workspaces: all_windows_of_workspaces,
+            all_normal_windows_of_workspaces: all_normal_windows_of_workspaces
+        });
     }
 
     // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsWorkspaces org.gnome.Shell.Extensions.GnomeUtilsWorkspaces.GoToGivenWorkspace int32:4
@@ -88,82 +165,5 @@ var WorkspaceFunctions = class WorkspaceFunctions {
         // Here 0 instead of global.get_current_time() will also work
         metaWorkspace.activate_with_focus(win, global.get_current_time());
         // win.activate_with_workspace(global.get_current_time(), metaWorkspace);
-    }
-
-    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsWorkspaces org.gnome.Shell.Extensions.GnomeUtilsWorkspaces.GetWorkspaceIndexByName string:"Codium"
-
-    GetWorkspaceIndexByName(workspaceName) {
-
-        // Get the total number of workspaces
-        let number_of_workspaces = global.workspace_manager.n_workspaces;
-
-        // Iterate through each workspace
-        for (let i = 0; i < number_of_workspaces; i++) {
-            // Get the workspace
-            const workspace = WorkspaceManager.get_workspace_by_index(i);
-
-            // Check if the workspace name matches
-            if (Meta.prefs_get_workspace_name(i) == workspaceName) {
-                // Return the index of the workspace
-                return JSON.stringify(i);
-            }
-        }
-    }
-
-    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsWorkspaces org.gnome.Shell.Extensions.GnomeUtilsWorkspaces.GetWorkspaces | jq .
-
-    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsWorkspaces org.gnome.Shell.Extensions.GnomeUtilsWorkspaces.GetWorkspaces | jq -r '.workspace_names[].name'
-
-    GetWorkspaces() {
-        // let w = global.workspace_manager;
-        // return JSON.stringify({
-        //     workspace_number: w.get_n_workspaces(),
-        //     workspace_index: w.get_active_workspace_index(),
-        //     windows: global.screen.get_active_workspace().list_windows(),
-        // });
-
-        let workspaces = []
-        let current_workspace = Meta.prefs_get_workspace_name(WorkspaceManager.get_active_workspace().index());
-        let number_of_workspaces = global.workspace_manager.n_workspaces;
-        let all_windows_of_workspaces = {};
-        let all_normal_windows_of_workspaces = {};
-        let sticky_windows = [];
-
-        for (let wks = 0; wks < number_of_workspaces; ++wks) {
-
-            // let temp = ;
-
-            workspaces.push({index: wks, name: Meta.prefs_get_workspace_name(wks)});
-
-
-
-            // let workspace_name = wks+'_'+Meta.prefs_get_workspace_name(wks);
-            let workspace_name = Meta.prefs_get_workspace_name(wks);
-
-            let metaWorkspace = global.workspace_manager.get_workspace_by_index(wks);
-            let all_windows = [];
-
-            metaWorkspace.list_windows().map(w => all_windows.push(w.get_id()));
-
-            // all_windows_of_workspaces.push({ [wks]: all_windows });
-            all_windows_of_workspaces[workspace_name] = all_windows;
-
-            let all_normal_windows = [];
-
-            metaWorkspace.list_windows().filter(w => w.get_window_type() == 0).map(w => all_normal_windows.push(w.get_id()));
-
-            // all_normal_windows_of_workspaces.push({ [wks]: all_normal_windows });
-            all_normal_windows_of_workspaces[workspace_name] = all_normal_windows;
-
-            metaWorkspace.list_windows().filter(w => w.get_window_type() == 0 && !w.is_skip_taskbar() && w.is_on_all_workspaces()).map(w => sticky_windows.push(w.get_id()));
-        }
-
-        return JSON.stringify({
-            workspaces: workspaces,
-            // number_of_workspaces: number_of_workspaces,
-            // current_workspace: current_workspace,
-            // all_windows_of_workspaces: all_windows_of_workspaces,
-            all_normal_windows_of_workspaces: all_normal_windows_of_workspaces
-        });
     }
 }
