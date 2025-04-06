66dae0666ebe56a2b9e6fab4791b7e8adb57b5f8
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-04-06 22:58:49 +0600

    Alhamdulillah, SetClipboard

--- clipboardFunctions.js
+++ clipboardFunctions.js
@@ -5,35 +5,36 @@ const Display = global.get_display();
 var MR_DBUS_IFACE = `
 <node>
    <interface name="org.gnome.Shell.Extensions.GnomeUtilsClipboard">
-      <method name="GetSelection">
-         <arg type="s" direction="out" name="selection" />
+      <method name="SetClipboard">
+         <arg type="s" direction="in" name="wm_class" />
       </method>
    </interface>
 </node>`;
 
 var clipboardFunctions = class clipboardFunctions {
 
-    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsClipboard org.gnome.Shell.Extensions.GnomeUtilsClipboard.GetSelection
+   // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsClipboard org.gnome.Shell.Extensions.GnomeUtilsClipboard.SetClipboard string:"firefox"
 
-    GetSelection() {
+   SetClipboard(input) {
         // Not Done
         // https://github.com/awamper/gpaste-integration
         // https://github.com/lsnow/translate-clipboard
         // https://github.com/tuberry/light-dict
         // https://github.com/eexpress/gs-clip-translator
-        let selection = Display.get_selection();
+      //   let selection = Display.get_selection();
         // https://stackoverflow.com/a/10548059/1772898
 
-        log(`Selection: ${selection}`);
 
-        // try {
-        //     let text = selection.get_text();
-        //     log(`Text of Selection is : ${error}`);
 
-        // }
-        // catch (error) {
-        //     log(`Error Text of Selection: ${error}`);
-        // }
+      //   log(`Selection: ${selection}`);
+
+        try {
+           St.Clipboard.get_default().set_text(St.ClipboardType.PRIMARY, input);
+           St.Clipboard.get_default().set_text(St.ClipboardType.CLIPBOARD, input);
+        }
+        catch (error) {
+            log(`Clipboard: ${error}`);
+        }
 
         // try {
         // let clipboard = St.Clipboard.get_default();
@@ -44,7 +45,6 @@ var clipboardFunctions = class clipboardFunctions {
         //     log(`Error GetSelection: ${error}`);
         // }
         // return selection;
-        return "selection";
     }
 }
 
