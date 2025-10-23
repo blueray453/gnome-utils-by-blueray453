ab99b7a02d737da2f3e615faee46f2b373fce0fa
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-10-24 05:32:43 +0600

    Alhamdulillah Added To appFunctions

--- appFunctions.js
+++ appFunctions.js
@@ -3,15 +3,15 @@ import Gio from 'gi://Gio';
 export const MR_DBUS_IFACE = `
 <node>
    <interface name="org.gnome.Shell.Extensions.GnomeUtilsApps">
-      <method name="GetAppDetails">
+      <method name="GetAppDetailsGivenAppID">
          <arg type="s" direction="in" name="appid" />
          <arg type="s" direction="out" name="app" />
       </method>
-      <method name="GetAppFromPID">
+      <method name="GetAppDetailsGivenPID">
          <arg type="u" direction="in" name="pid" />
          <arg type="s" direction="out" name="app" />
       </method>
-      <method name="GetAppFromFocusedWindow">
+      <method name="GetAppDetailsGivenFocusedWindow">
          <arg type="s" direction="out" name="app" />
       </method>
       <method name="GetAppGivenWindowID">
@@ -30,11 +30,9 @@ export class AppFunctions {
         return app;
     }
 
-    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsApps org.gnome.Shell.Extensions.GnomeUtilsApps.GetAppDetails string:"io.github.cboxdoerfer.FSearch.desktop" | jq .
-
-    GetAppDetails(app_id) {
-        let desktop_apps = Gio.DesktopAppInfo.new(app_id);
+    _get_properties_brief_given_app_id = function (app_id) {
         let shell_apps = global.get_app_system().lookup_app(app_id);
+        let desktop_apps = shell_apps.get_app_info();
 
         // get_display_name is a function of AppInfo which is DesktopAppInfo inherited
 
@@ -48,50 +46,56 @@ export class AppFunctions {
         })
 
         if (app_id) {
-            let icon_val = "";
-
-            if (shell_apps.get_icon()) {
-                icon_val = shell_apps.get_icon().to_string();
-            }
-            return JSON.stringify({
+            return {
                 app_name: desktop_apps.get_name(),
                 app_file_name: desktop_apps.get_filename(),
                 app_display_name: desktop_apps.get_display_name(),
                 app_id: desktop_apps.get_id(),
                 wm_class: desktop_apps.get_startup_wm_class(),
                 app_pids: shell_apps.get_pids(),
-                app_icon: icon_val,
+                app_icon: shell_apps.get_icon()?.to_string(),
                 app_windows_number: shell_apps.get_n_windows(),
                 app_windows: windows_array,
                 state: shell_apps.get_state(),
-                commandline: shell_apps.get_commandline()
-            });
+                description: shell_apps.get_description(),
+                commandline: desktop_apps.get_commandline(),
+                executable: desktop_apps.get_executable(),
+            };
         } else {
             throw new Error('Not found');
         }
     }
 
-    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsApps org.gnome.Shell.Extensions.GnomeUtilsApps.GetAppFromPID uint32:3931313482
+    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsApps org.gnome.Shell.Extensions.GnomeUtilsApps.GetAppDetailsGivenAppID string:"io.github.cboxdoerfer.FSearch.desktop" | jq .
 
-    GetAppFromPID(pid) {
+    GetAppDetailsGivenAppID(app_id) {
+        return JSON.stringify(this._get_properties_brief_given_app_id(app_id));
+    }
+
+    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsApps org.gnome.Shell.Extensions.GnomeUtilsApps.GetAppDetailsGivenPID uint32:3931313482 | jq .
+
+    GetAppDetailsGivenPID(pid) {
         let tracker = global.get_window_tracker();
         let app = tracker.get_app_from_pid(pid);
-        return app.get_id();
+        return JSON.stringify(this._get_properties_brief_given_app_id(app.get_id()));
     }
 
-    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsApps org.gnome.Shell.Extensions.GnomeUtilsApps.GetAppFromFocusedWindow
+    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsApps org.gnome.Shell.Extensions.GnomeUtilsApps.GetAppDetailsGivenFocusedWindow | jq .
 
-    GetAppFromFocusedWindow() {
-        let win = global.get_window_actors().find(w => w.meta_window.has_focus() == true);
-        // let wmclass = w.meta_window.get_wm_class();
-        // return Gio.AppInfo.get_all().find(a => a.get_startup_wm_class() == wmclass).get_id();
-
+    GetAppDetailsGivenFocusedWindow() {
         let tracker = global.get_window_tracker();
-        let app = tracker.get_window_app(win.meta_window);
-        return app.get_id();
+        let app = tracker.get_focus_app();
+        return JSON.stringify(this._get_properties_brief_given_app_id(app.get_id()));
+        // let win = global.get_window_actors().find(w => w.meta_window.has_focus() == true);
+        // // let wmclass = w.meta_window.get_wm_class();
+        // // return Gio.AppInfo.get_all().find(a => a.get_startup_wm_class() == wmclass).get_id();
+
+        // let tracker = global.get_window_tracker();
+        // let app = tracker.get_window_app(win.meta_window);
+        // return app.get_id();
     }
 
-    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsApps org.gnome.Shell.Extensions.GnomeUtilsApps.GetAppGivenWindowID uint32:44129093
+    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsApps org.gnome.Shell.Extensions.GnomeUtilsApps.GetAppGivenWindowID uint32:44129093  | jq .
 
     GetAppGivenWindowID(winid) {
 
@@ -101,39 +105,28 @@ export class AppFunctions {
         //   return global.get_app_system().lookup_app(app_id).get_icon().to_string();
         let tracker = global.get_window_tracker();
         let app = tracker.get_window_app(w.meta_window);
-        return app.get_id();
+        return JSON.stringify(this._get_properties_brief_given_app_id(app.get_id()));
     }
 
     // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsApps org.gnome.Shell.Extensions.GnomeUtilsApps.GetRunningApps | jq .
 
     GetRunningApps() {
         let apps = global.get_app_system().get_running();
+        let results = [];
 
-        var appsJsonArr = [];
-        apps.forEach(function (a) {
-            let icon_val = "";
-
-            if (a.get_icon()) {
-                icon_val = a.get_icon().to_string();
+        apps.forEach(app => {
+            let app_id = app.get_id();
+            try {
+                let info = this._get_properties_brief_given_app_id(app_id);
+                results.push(info);
+            } catch (err) {
+                results.push({ app_id, error: err.message });
             }
-            // var windows = a.get_windows();
-            // console.log("windows : " + windows);
+        });
 
-            // if (windows){
-            //     windows.forEach(function (w) {
-            //         console.log("window id : " + w.get_id());
-            //     });
-            // }
-            appsJsonArr.push({
-                app_name: a.get_name(),
-                app_id: a.get_id(),
-                app_pids: a.get_pids(),
-                app_icons: icon_val
-            });
 
-        })
 
-        return JSON.stringify(appsJsonArr);
+        return JSON.stringify(results);
     }
 }
 
