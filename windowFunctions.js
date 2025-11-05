d977df43a78a2d707bff2d03420ddc55fb18d801
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-11-05 15:39:03 +0600

    Alhamdulillah, Working on GetWindowsExcludingGivenWMClass

--- windowFunctions.js
+++ windowFunctions.js
@@ -271,6 +271,10 @@ export class WindowFunctions {
         return this._get_normal_windows_current_workspace().filter(w => w.get_wm_class() == wm_class);
     }
 
+    _get_normal_windows_excluding_given_wm_classes(wm_classes) {
+        return this._get_normal_windows().filter(w => !wm_classes.includes(w.get_wm_class()))
+    }
+
     _get_normal_window_given_window_id = function (win_id) {
         // find does not need filtered window. As it return one result. However keeping it for readability
         let win = this._get_normal_windows().find(w => w?.get_id() == win_id);
@@ -571,14 +575,13 @@ export class WindowFunctions {
         return JSON.stringify(winPropertiesArr);
     }
 
-    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsWindows org.gnome.Shell.Extensions.GnomeUtilsWindows.GetWindowsExcludingGivenWMClass array:string:"io.github.cboxdoerfer.FSearch","VSCodium","firefox-esr","Nemo","Alacritty" | jq .
+    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsWindows org.gnome.Shell.Extensions.GnomeUtilsWindows.GetWindowsExcludingGivenWMClass array:string:"Io.github.cboxdoerfer.FSearch","VSCodium","firefox-esr","Nemo","Alacritty" | jq .
 
     GetWindowsExcludingGivenWMClass(wm_classes) {
-        // 1) Get all normal windows
-        let wins = this._get_normal_windows();
+        let wins = this._get_normal_windows_excluding_given_wm_classes(wm_classes);
 
-        // 2) Filter out windows whose wm_class is listed in wm_classes
-        let winPropertiesArr = wins.filter(w => !wm_classes.includes(w.get_wm_class())).map(win => this._get_properties_brief_given_meta_window(win));
+        // Map each window to its properties
+        let winPropertiesArr = wins.map(win => this._get_properties_brief_given_meta_window(win));
 
         return JSON.stringify(winPropertiesArr);
     }
