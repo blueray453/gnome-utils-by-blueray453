cafcb2d5626758c5b828c562cb407e06930b9f12
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-12-04 08:51:22 +0600

    Alhamdulillah, add keys

--- keyboardSimulatorFunctions.js
+++ keyboardSimulatorFunctions.js
@@ -1,11 +1,13 @@
 import Clutter from 'gi://Clutter';
-import Gio from 'gi://Gio';
-import GLib from 'gi://GLib';
 
 export const MR_DBUS_IFACE = `
 <node>
    <interface name="org.gnome.Shell.Extensions.GnomeUtilsKeyboardSimulator">
         <method name="EmitMetaO">
+        </method>
+        <method name="Equal">
+        </method>
+        <method name="Minus">
         </method>
    </interface>
 </node>`;
@@ -17,30 +19,11 @@ export class KeyboardSimulatorFunctions {
     EmitMetaO() {
         // // Delay to ensure VS Code window is focused
         // GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1000, () => {
-            const VirtualKeyboard = Clutter.get_default_backend()
-                .get_default_seat()
-                .create_virtual_device(Clutter.InputDeviceType.KEYBOARD_DEVICE);
+        const VirtualKeyboard = Clutter.get_default_backend()
+            .get_default_seat()
+            .create_virtual_device(Clutter.InputDeviceType.KEYBOARD_DEVICE);
 
-            const eventTime = Clutter.get_current_event_time() * 1000;
-
-            // // Release stuck modifiers
-            // const stuckModifiers = [
-            //     Clutter.KEY_Shift_L, Clutter.KEY_Shift_R,
-            //     Clutter.KEY_Control_L, Clutter.KEY_Control_R,
-            //     Clutter.KEY_Alt_L, Clutter.KEY_Alt_R,
-            //     Clutter.KEY_Super_L, Clutter.KEY_Super_R
-            // ];
-            // stuckModifiers.forEach(key => {
-            //     VirtualKeyboard.notify_keyval(eventTime, key, Clutter.KeyState.RELEASED);
-            // });
-
-            // // Press Meta+O
-            // VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_Super_L, Clutter.KeyState.PRESSED);
-            // VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_o, Clutter.KeyState.PRESSED);
-            // VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_o, Clutter.KeyState.RELEASED);
-            // VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_Super_L, Clutter.KeyState.RELEASED);
-
-        // Press all modifiers first
+        const eventTime = Clutter.get_current_event_time() * 1000;
         // ctrl+shift+alt+meta+insert
         VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_Control_L, Clutter.KeyState.PRESSED);
         VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_Shift_L, Clutter.KeyState.PRESSED);
@@ -52,8 +35,31 @@ export class KeyboardSimulatorFunctions {
         VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_Alt_L, Clutter.KeyState.RELEASED);
         VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_Shift_L, Clutter.KeyState.RELEASED);
         VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_Control_L, Clutter.KeyState.RELEASED);
+    }
 
-        //     return GLib.SOURCE_REMOVE;
-        // });
+    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsKeyboardSimulator org.gnome.Shell.Extensions.GnomeUtilsKeyboardSimulator.Equal
+
+    Equal() {
+        const VirtualKeyboard = Clutter.get_default_backend()
+            .get_default_seat()
+            .create_virtual_device(Clutter.InputDeviceType.KEYBOARD_DEVICE);
+
+        const eventTime = Clutter.get_current_event_time() * 1000;
+
+        VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_equal, Clutter.KeyState.PRESSED);
+        VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_equal, Clutter.KeyState.RELEASED);
+    }
+
+    // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsKeyboardSimulator org.gnome.Shell.Extensions.GnomeUtilsKeyboardSimulator.Minus
+
+    Minus() {
+        const VirtualKeyboard = Clutter.get_default_backend()
+            .get_default_seat()
+            .create_virtual_device(Clutter.InputDeviceType.KEYBOARD_DEVICE);
+
+        const eventTime = Clutter.get_current_event_time() * 1000;
+
+        VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_minus, Clutter.KeyState.PRESSED);
+        VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_minus, Clutter.KeyState.RELEASED);
     }
 }
