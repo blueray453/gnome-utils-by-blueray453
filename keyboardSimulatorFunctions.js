ea3681968076b073ec3d4e3296b725ed36c932c3
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-12-04 09:23:04 +0600

    Alhamdulillah, refactor

--- keyboardSimulatorFunctions.js
+++ keyboardSimulatorFunctions.js
@@ -67,6 +67,27 @@ export class KeyboardSimulatorFunctions {
     //     ]);
     // }
 
+    // EmitMetaO() {
+    //     // // Delay to ensure VS Code window is focused
+    //     // GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1000, () => {
+    //     const VirtualKeyboard = Clutter.get_default_backend()
+    //         .get_default_seat()
+    //         .create_virtual_device(Clutter.InputDeviceType.KEYBOARD_DEVICE);
+
+    //     const eventTime = Clutter.get_current_event_time() * 1000;
+    //     // ctrl+shift+alt+meta+insert
+    //     VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_Control_L, Clutter.KeyState.PRESSED);
+    //     VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_Shift_L, Clutter.KeyState.PRESSED);
+    //     VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_Alt_L, Clutter.KeyState.PRESSED);
+    //     VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_Super_L, Clutter.KeyState.PRESSED);
+    //     VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_o, Clutter.KeyState.PRESSED);
+    //     VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_o, Clutter.KeyState.RELEASED);
+    //     VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_Super_L, Clutter.KeyState.RELEASED);
+    //     VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_Alt_L, Clutter.KeyState.RELEASED);
+    //     VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_Shift_L, Clutter.KeyState.RELEASED);
+    //     VirtualKeyboard.notify_keyval(eventTime, Clutter.KEY_Control_L, Clutter.KeyState.RELEASED);
+    // }
+
     // // dbus-send --print-reply=literal --session --dest=org.gnome.Shell /org/gnome/Shell/Extensions/GnomeUtilsKeyboardSimulator org.gnome.Shell.Extensions.GnomeUtilsKeyboardSimulator.Equal
 
     // Equal() {
