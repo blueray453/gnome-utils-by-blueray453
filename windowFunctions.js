c2f8f44341dcfb1a3c4424b17358e5f5befb2afe
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2026-09-01 20:14:18 +0600

    Alhamdulillah, working

--- windowFunctions.js
+++ windowFunctions.js
@@ -1174,16 +1174,12 @@ export class WindowFunctions {
     WindowsCloseDuplicateNemo() {
         let wins = this._get_normal_windows_current_workspace_given_wm_class(NEMO);
 
-        wins.forEach(function (w) {
-            if (w.get_wm_class_instance() == 'file_progress') {
-                return; // Skip this window if it's a 'file_progress' instance
-            }
-
-            w.delete(0);
-        })
-
         let seen = {};
         wins.forEach(win => {
+            if (win.get_wm_class_instance() == 'file_progress') {
+                return; // Never touch progress dialogs
+            }
+
             let key = win.get_title();
             if (!seen[key]) {
                 seen[key] = win;
